ad99e8fd13ba135faeb750c6b56e1c6e4fff5df4
RicardoD4W <ricardo.rodriguez@fernando3martos.com>
2023-05-06 19:09:29 +0200

    middleware routes

--- src/pages/user/AdminPage.jsx
+++ src/pages/user/AdminPage.jsx
@@ -1,5 +1,20 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useMainStore } from '../../stores/mainContext'
+
 const AdminPage = () => {
-	return <>user page</>
+	const user = useMainStore((state) => state.user)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		!user.email &&
+			user.roles.map((rol) => {
+				return rol === 'ADMIN_ROLE'
+			}) &&
+			navigate('/iniciar-sesion')
+	}, [])
+
+	return <>admin page</>
 }
 
 export default AdminPage
--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useMainStore } from '../../stores/mainContext'
+
 const UserPage = () => {
+	const user = useMainStore((state) => state.user)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		!user.email &&
+			user.roles.map((rol) => {
+				return rol === 'USER_ROLE'
+			}) &&
+			navigate('/iniciar-sesion')
+	}, [])
+
 	return <>user page</>
 }
 
